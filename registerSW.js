d7729fa0f75e68617e11c91d883352239bcb9f76
vidalh01 <vidalherrera01ec@gmail.com>
2025-03-16 22:24:45 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/page-rutadiario/sw.js', { scope: '/page-rutadiario/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/rutadiario/sw.js', { scope: '/rutadiario/' })})}
